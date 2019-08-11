74d610b36079ab75848ac1338dfb4f884dcd9543
Mauricio Reyes <mreyes@talpor.com>
2019-08-11 15:22:07 -0400

    tslint

--- src/store/games/actions.ts
+++ src/store/games/actions.ts
@@ -3,13 +3,13 @@ import { IActionGroup, IStore } from '../index';
 import { IGame } from './store';
 
 const actions: IActionGroup = {
-  listUpcoming: async (_state: IStore) => {
-    const upcoming: IGame[] = await (await api.gamesList('upcoming')()).json();
-    return { games: { upcoming } };
-  },
   list: async (_state: IStore, tournament: string) => {
     const games: IGame[] = await (await api.gamesList(tournament)()).json();
     return { games: { [tournament]: games } };
+  },
+  listUpcoming: async (_state: IStore) => {
+    const upcoming: IGame[] = await (await api.gamesList('upcoming')()).json();
+    return { games: { upcoming } };
   }
 };
 
