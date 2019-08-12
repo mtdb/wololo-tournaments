827256d5156d2f5fc9ea269b04162ece00d84f35
Mauricio Reyes <mreyes@talpor.com>
2019-08-11 21:51:02 -0400

    minor design changes

--- src/scenes/Games/Games.tsx
+++ src/scenes/Games/Games.tsx
@@ -1,10 +1,11 @@
-import { navigate } from '@reach/router';
+import { Link } from '@reach/router';
 import 'flag-icon-css/css/flag-icon.css';
 import React, { useEffect } from 'react';
 import { routes } from '../../App';
 import { withContext } from '../../contrib/context';
 import { IActions } from '../../store';
 import { IGame } from '../../store/games/store';
+import moment from 'moment';
 
 import './Games.scss';
 
@@ -25,38 +26,48 @@ const GamesComponent = ({
     } else {
       list(tournament);
     }
-  }, [listUpcoming, tournament]);
+  }, [list, listUpcoming, tournament]);
   const filter = tournament || 'upcoming';
   return (
     <div id="Games">
       {games[filter] &&
         games[filter].map(game => (
-          <div
-            className="game-summary"
-            key={game.slug}
-            onClick={() => {
-              navigate(routes.game(game.slug));
-            }}
-          >
-            <div className="header">{game.tournament}</div>
-            {game.players && game.players.length === 2 && (
-              <div className="content">
-                <div
-                  className={`flag-icon flag-icon-${game.players[0].player.country.toLowerCase()} left`}
-                />
-                <div className="inner-content">
-                  <div className="players">
-                    <div className="player-name p1">{game.players[0].player.name}</div>
-                    <div className="vs">vs</div>
-                    <div className="player-name p2">{game.players[1].player.name}</div>
+          <div className="game-summary" key={game.slug}>
+            <div className="header">
+              <Link to={routes.tournamentGames(game.tournament.slug)}>{game.tournament.name}</Link>
+            </div>
+            <Link to={routes.game(game.slug)}>
+              {(game.players && game.players.length) === 2 ? (
+                <div className="content">
+                  <div
+                    className={`flag-icon flag-icon-${game.players[0].player.country.toLowerCase()} left`}
+                  />
+                  <div className="inner-content">
+                    <div className="players">
+                      <div className="player-name p1">{game.players[0].player.name}</div>
+                      <div className="vs">vs</div>
+                      <div className="player-name p2">{game.players[1].player.name}</div>
+                    </div>
                   </div>
+                  <div
+                    className={`flag-icon flag-icon-${game.players[1].player.country.toLowerCase()} right`}
+                  />
                 </div>
-                <div
-                  className={`flag-icon flag-icon-${game.players[1].player.country.toLowerCase()} right`}
-                />
-              </div>
-            )}
-            <div className="footer">{game.date}</div>
+              ) : (
+                <div className="content">
+                  <div className={`flag-icon left`} />
+                  <div className="inner-content">
+                    <div className="players">
+                      <div className="player-name p1">???</div>
+                      <div className="vs">vs</div>
+                      <div className="player-name p2">???</div>
+                    </div>
+                  </div>
+                  <div className={`flag-icon right`} />
+                </div>
+              )}
+            </Link>
+            <div className="footer">{moment(game.date).calendar()}</div>
           </div>
         ))}
       {(!games[filter] || !games[filter].length) && <div>loading...</div>}
